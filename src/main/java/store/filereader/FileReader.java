2ca98954e8f63bdf1ffda10ed0bd2ab7894ee002
DongYeop <s01054830455@gmail.com>
2024-11-09 21:39:11 +0900

    feat: 프로모션 정보를 읽어오는 기능 추가
    - promotions.md 에서 프로모션 정보를 읽어온다.

--- src/main/java/store/filereader/FileReader.java
+++ src/main/java/store/filereader/FileReader.java
@@ -3,10 +3,14 @@ package store.filereader;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import store.model.Product;
 import store.model.Products;
+import store.model.Promotion;
+import store.model.Promotions;
 
 public class FileReader {
 
@@ -37,5 +41,32 @@ public class FileReader {
             System.out.println("파일을 읽는 중 오류가 발생했습니다: " + e.getMessage());
         }
         return new Products(products);
+    }
+
+    public static Promotions readerPromotionFile() {
+        List<Promotion> promotions = new ArrayList<>();
+
+        File file = new File("src/main/resources/promotions.md");
+
+        try (BufferedReader br = new BufferedReader(new java.io.FileReader(file))) {
+            String line;
+
+            br.readLine();
+            while ((line = br.readLine()) != null) {
+                String[] fields = line.split(",");
+
+                String name = fields[0];
+                int buy = Integer.parseInt(fields[1]);
+                int get = Integer.parseInt(fields[2]);
+
+                LocalDateTime startDate = LocalDate.parse(fields[3]).atStartOfDay();
+                LocalDateTime endDate = LocalDate.parse(fields[4]).atStartOfDay();
+
+                promotions.add(new Promotion(name, buy, get, startDate, endDate));
+            }
+        } catch (IOException e) {
+            System.out.println("파일을 읽는 중 오류가 발생했습니다: " + e.getMessage());
+        }
+        return new Promotions(promotions);
     }
 }
---
+++ src/main/java/store/model/Promotion.java
@@ -0,0 +1,24 @@
+package store.model;
+
+import java.time.LocalDateTime;
+
+public class Promotion {
+
+    private final String name;
+    private final int buy;
+    private final int get;
+    private final LocalDateTime startDate;
+    private final LocalDateTime endDate;
+
+    public Promotion(String name, int buy, int get, LocalDateTime startDate, LocalDateTime endDate) {
+        this.name = name;
+        this.buy = buy;
+        this.get = get;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
